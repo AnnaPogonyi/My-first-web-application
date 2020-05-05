ea88bd04be9da871d75b79094b85fee4321cffc5
Anna <anna.pogonyi@gmail.com>
2020-05-05 15:39:27 +0200

    first picture added

---
+++ script.js
@@ -0,0 +1,12 @@
+let photo = 'images/autumn.jpg';
+let title = 'Autumn';
+let description = 'Autumn marks the transition from summer to winter.';
+
+let data = {
+    photo = 'images/autumn.jpg',
+    title = 'Autumn',
+    description = 'Autumn marks the transition from summer to winter.'
+
+}
+
+$('#photo').attr('src', data.photo);
