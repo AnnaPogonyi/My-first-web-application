02cd49399c6f729d481b1f924332c909f43cc005
Anna <anna.pogonyi@gmail.com>
2020-05-05 16:59:09 +0200

    nem megy

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ let title = 'Autumn';
 let description = 'Autumn marks the transition from summer to winter.';
 
 let data = {
-    photo = 'images/autumn.jpg',
-    title = 'Autumn',
-    description = 'Autumn marks the transition from summer to winter.'
+    photo : 'images/autumn.jpg',
+    title : 'Autumn',
+    description : 'Autumn marks the transition from summer to winter.'
 
-}
+};
 
 $('#photo').attr('src', data.photo);
+
+
+
